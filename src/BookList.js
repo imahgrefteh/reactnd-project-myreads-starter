c5c5526c5b299f086837a25ea57fea8e4808bd35
isaac <isaac@isaks-mbp.lan>
2021-07-29 21:35:12 -0700

    removed { from screen

--- src/BookList.js
+++ src/BookList.js
@@ -16,7 +16,6 @@ function BookList(props) {
                     </li>
                 );
             })}
-            }
         </ol>
     );
 }
--- src/Shelf.js
+++ src/Shelf.js
@@ -6,7 +6,7 @@ class Shelf extends Component {
     render() {
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.bookShelfTitle}</h2>
+                <h2 className="bookshelf-title">{this.props.Title}</h2>
                 <div className="bookshelf-books">
                     <BookList books={this.props.books} />
                 </div>
